8a520a1972eb36cb0b45011ce35da61d8436b7ed
Soab Mahmud Syfuddhin <108150913+Soab42@users.noreply.github.com>
2024-04-19 18:57:24 +0000

    test

--- src/components/Library.tsx
+++ src/components/Library.tsx
@@ -3,13 +3,13 @@ import Books from './Books'
 import SearchInput from './SearchInput'
 
 const Library = async () => {
-  const { data: books } = await getBooks()
-
+  const data = await getBooks()
+console.log(data);
   return (
     <div className='h-96 mt-6 p-2 flex flex-col overflow-auto gap-2 w-[23rem] rounded-md bg-green-300'>
       <h1 className='border-b-black border-b font-semibold'>Books</h1>
       <SearchInput />
-      <Books books={books} />
+      <Books books={data?.books} />
     </div>
   )
 }
