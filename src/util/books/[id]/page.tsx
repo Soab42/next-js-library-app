975fc26b7bdb16b758a83215414cfee94c091bde
Novo1999 <neptune.navo@gmail.com>
2024-04-19 21:04:19 +0600

    url change

--- src/util/books/[id]/page.tsx
+++
@@ -1,7 +0,0 @@
-import { getBook } from '@/lib/book-data'
-
-const Book = async ({ params: { id } }: { params: { id: string } }) => {
-  const { book } = await getBook(id)
-  return <div>Book</div>
-}
-export default Book
